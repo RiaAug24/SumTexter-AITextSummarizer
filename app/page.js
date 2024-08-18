60efaba04fb3847299aa96211c9d0441e5774846
Riyaz Ahmed <riyaz04ahmed24aug.github@gmail.com>
2024-08-18 21:43:44 +0530

    Updated UI

--- app/page.js
+++ app/page.js
@@ -96,36 +96,40 @@ export default function Home() {
   return (
     <>
       <div className="mx-auto p-8 grid justify-center grid-rows-3 gap-20">
-        <h1 className="text-[1.85rem] sm:text-4xl xl:text-5xl font-bold bg-gradient-to-tr from-pink-600 to-violet-400 via-rose-400 inline-block text-transparent bg-clip-text">
+        <h1 className="text-center text-[1.85rem] sm:text-4xl xl:text-5xl font-bold bg-gradient-to-tr from-pink-600 to-violet-400 via-rose-400 inline-block text-transparent bg-clip-text">
           SumTexter - AI PDF Summarizer
         </h1>
-        <h2 className="text-center text-[1.25rem] sm:text-3xl xl:text-5xl font-bold bg-gradient-to-r from-gray-100 to-violet-400 via-pink-600 inline-block text-transparent bg-clip-text">
+
+
+        <div className="grid grid-rows-2 gap-8">
+        <h2 className="text-center text-[1rem] sm:text-2xl lg:text-3xl font-bold bg-gradient-to-r from-gray-100 to-violet-400 via-pink-600 inline-block text-transparent bg-clip-text">
           Upload PDF
         </h2>
     
         <input
-          className="w-1/2 p-2 mx-auto border-2 border-purple-300"
+          className="rounded-lg w-3/4 p-2 md:h-[75px]h-auto mx-auto border-2 border-purple-300"
           type="file"
           id="file"
           name="file"
           onChange={onFileChange}
           accept=".pdf"
-        />
+        /></div>
+        <div className="grid grid-rows-2">
+        <p className="text-center text-[1.15rem] text-gray-200 mb-4">Choose the page no of PDF to Summarize</p>
         <input
-          className="text-center w-1/2 p-2 text-slate-900 mx-auto border-2 border-purple-300"
+          className="rounded-lg text-center md:max-w-[180px] w-1/2 h-1/2 p-2 md:p-4 bg-purple-300 text-slate-800 mx-auto border-2 border-purple-300"
           type="number"
           min={1}
-          placeholder="Choose the page no of PDF to Summarize"
           onChange={selectPageNo} // Trigger summarization when page number changes
-        />
+        /></div>
       </div>
       <h2 className="text-center text-[1.5rem] text-gray-200 mt-3 mb-6">
         PDF Summary 👇
       </h2>
-      <p className="max-w-md mx-auto border-2 border-purple-400 p-4">
+      <p className="rounded-lg lg:max-w-xl w-3/4 mx-auto border-2 border-purple-400 py-2 px-3">
         {summary}
       </p>
-      <p className="text-center my-24">
+      <p className="text-center md:text-[1.25rem] text-sm my-24 ">
         Made with ❤️<br />
         By Riyaz Ahmed
       </p>
